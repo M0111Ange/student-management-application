3d9efae5513d3d143a04672dee503848cf2383a0
M0111Ange <angemukundente@gmail.com>
2023-11-30 09:42:40 +0200

    student managament application

---
+++ database/students.js
@@ -0,0 +1,33 @@
+const students = [
+    {
+        id: 1,
+        name: "Ange Mukundente",
+        age: 20,
+        email: "ange@gmail.com",
+        gender: "Female",
+    },
+    {
+        id: 2,
+        name: "Isaac Mukunzi",
+        age: 23,
+        email: "mukunzi@gmail.com",
+        gender: "Male", 
+    },
+    {
+        id: 3,
+        name: "Ruth Uwamahoro",
+        age: 21,
+        email: "ruth@gmail.com",
+        gender: "Female",
+    },
+    {
+        id: 4,
+        name: "Elie Mugisha",
+        age: 23,
+        email: "mugisha@gmail.com",
+        gender: "Male",
+    },
+];
+
+module.exports = {
+    students};
